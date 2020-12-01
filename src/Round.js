86102c0ed9a2e5874e67e6841974e9da035595aa
Chris Spohn <chris.j.spohn@gmail.com>
2020-11-30 20:40:04 -0700

    Complete Round class based on rubric description

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,39 @@
 class Round {
-  constructor() {
-
+  constructor(deck) {
+    this.deck = deck,
+    this.turns = 0,
+    this.incorrectGuesses = [],
   }
+
+  returnCurrentCard() {
+    currentCard = this.deck[0]
+    return currentCard;
+  }
+
+  takeTurn(guess) {
+    this.turns++;
+
+    let card = returnCurrentCard();
+    let turn = new Turn(guess, card);
+
+    this.deck.shift();
+    this.deck.push(card);
+
+    if (!turn.evaluateGuess()) {
+      this.incorrentGuesses.push(card.id);
+    }
+
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    return (this.incorrectGuesses.length / this.deck.length) * 100;
+  }
+
+  endRound() {
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+  }
+
 }
 
 
