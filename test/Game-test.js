1cfd2f869d1fd5cf8aa02f7edb1570265c9db43e
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-02 13:16:35 -0700

    Add test descriptions

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,7 +8,25 @@ const Turn = require('../src/Turn');
 const Game = require('../src/Game');
 
 describe('Game', function() {
-  
 
+  beforeEach(function() {
+    
+  })
+
+  it('should keep track of the current round', function() {
+
+  });
+
+  it('should create cards', function() {
+
+  });
+
+  it('should be able to put cards in a deck', function() {
+
+  });
+
+  it('should create a new round using the deck', function() {
+
+  });
 
 });
