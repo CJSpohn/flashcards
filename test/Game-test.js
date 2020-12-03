88d1b606ab90821f754de8b521efe7a9e66e9eb6
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-03 16:04:43 -0700

    Lint game-test

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,23 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Round = require('../src/Round');
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 const Game = require('../src/Game');
-const data = require('../src/data');
-const questions = data.prototypeData;
+const Round = require('../src/Round');
 
 describe('Game', () => {
-  let deck, turn, round;
+  let game;
 
   beforeEach(() => {
-    let card1 = new Card(1, 'What is the air speed velocity of an unlaiden swallow?', ['Umm, I don\'t know', '42 mph', 'fast'], 'Umm, I don\'t know');
-    let card2 = new Card(2, 'What is Chris\'s favorite movie?', ['Cool Runnings', 'Tropic Thunder', 'The Way, Way Back'], 'The Way, Way Back');
-    let card3 = new Card(3, 'What card is this?', ['1', '2', '3'], '3');
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
     game = new Game();
   });
 
